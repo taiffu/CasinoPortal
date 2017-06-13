2e4b2c08835d4a4e48d94856ff0023cf3b97afe7
davy42 <davydov_alexander@bk.ru>
2017-06-13 15:11:32 +0300

    fixed faucet timer

--- js/balance.js
+++ js/balance.js
@@ -3,8 +3,7 @@
 			$('#bg_popup.reg').show().find('h1').html('Please, sign in on the <a href="' + window.location.origin + window.location.search + '">Platform</a>');
 		} else {
 			getTxList(10);
-			console.log("!!!!!!!!!!!!!!")
-			//$('#bg_popup.faucet').show()
+			$('#bg_popup.faucet').show()
 			animateTimer(60);
 			var openkey = localStorage.getItem("openkey");
 			var clipboard = new Clipboard('#myado');
@@ -13,7 +12,6 @@
 				e.preventDefault();
 				sendMoney();
 			});
-
 			$.get("https://platform.dao.casino/api/?a=faucet&to=" + openkey);
 			$.ajax({
 				url: urlInfura,
@@ -35,7 +33,8 @@
 						console.log("___send_adviser_And_Operator__")
 						sendRefAndOperator();
 					} else {
-						setTimeout(function(){$('#bg_popup.faucet').hide()},20000);
+
+						checkBet();
 					}
 				}
 			})
@@ -48,27 +47,39 @@
 			$("#timer").html(time + " second");
 			time--;
 			if (time < 0) {
-			console.log("exit")
-			clearInterval(t);
-			 $('#bg_popup').hide();
-		}
+				clearInterval(t);
+				$('#bg_popup').hide();
+			}
 		}, 1000)
 	}
 
-	function getTxList(count){
-		$.get("http://ropsten.etherscan.io/api?module=account&action=txlist&address=" + openkey + "&startblock=0&endblock=latest&", function (d){
-			for(var n = 0; n < Math.min(d.result.length,count); n++){
+	function getTxList(count) {
+		$.get("http://ropsten.etherscan.io/api?module=account&action=txlist&address=" + openkey + "&startblock=0&endblock=latest&", function (d) {
+			for (var n = 0; n < Math.min(d.result.length, count); n++) {
 				var r = d.result[n];
-				    $("tbody").prepend([
-						'<tr>'
-						+'<td><a target="_blank" href="https://ropsten.etherscan.io/tx/'+r.hash+'">'+r.hash.substr(0,16)+'</a></td>'
-						+'<td><a target="_blank" href="https://ropsten.etherscan.io/address/'+r.from+'">'+r.from.substr(0,16)+'</a></td>'
-						+'<td><a target="_blank" href="https://ropsten.etherscan.io/address/'+r.to+'">'+r.to.substr(0,16)+'</a></td>'
-						+'<td>'+(r.value)/1000000000000000000+' ETH</td>'
-  						].join(''));
-				console.log(d.result[n])
+				$("tbody").prepend([
+					'<tr>' +
+					'<td><a target="_blank" href="https://ropsten.etherscan.io/tx/' + r.hash + '">' + r.hash.substr(0, 16) + '</a></td>' +
+					'<td><a target="_blank" href="https://ropsten.etherscan.io/address/' + r.from + '">' + r.from.substr(0, 16) + '</a></td>' +
+					'<td><a target="_blank" href="https://ropsten.etherscan.io/address/' + r.to + '">' + r.to.substr(0, 16) + '</a></td>' +
+					'<td>' + (r.value) / 1000000000000000000 + ' ETH</td>'
+				].join(''));
 			}
 		})
 	}
 
-	
+	function checkBet() {
+		var b = parseFloat($('#balance').html().replace(" BET", ""));
+		console.log("b: " + b)
+		setTimeout(function () {
+			var n = parseFloat($('#balance').html().replace(" BET", ""));
+			console.log("n: " + n)
+			if (b != n && !isNaN(b)) {
+				$('#bg_popup.faucet').hide();
+				return;
+			} else {
+				checkBet();
+			}
+		}, 2000)
+
+	};
