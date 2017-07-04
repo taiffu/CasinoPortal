41e43fe6b2c08576e3a100b12a09323d610d3b45
AlexStep <alex.step.kellas@gmail.com>
2017-07-04 17:22:18 +0200

    bugfix

--- js/balance.js
+++ js/balance.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 	if (!localStorage.getItem('keystore')) {
-		$('#bg_popup.reg').show().find('h1').html('Please, sign in on the <a href="' + window.location.origin + window.location.search + '">Platform</a>');
+		$('#bg_popup.reg').show().find('h1').html('Please, sign in on the <a href="https://platform.dao.casino">Platform</a>');
 		return
 	}
 	var openkey = localStorage.getItem("openkey");
